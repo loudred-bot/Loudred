b5c028fc09fbb3d1c817be764b7af09e7abe2b82
Blair Wilcox <reccanti@gmail.com>
2020-08-27 23:18:55 -0400

    Make streams faster by cutting out ffmpeg
    I found out that the "play" method of the VoiceConnection object accepts
    a "converted" type, which accepts the type of audio that PortAudio
    returns by default. Therefore, there's no need to to use ffmpeg to
    convert it.

--- src/streamManager.js
+++ src/streamManager.js
@@ -1,18 +1,11 @@
 const { PassThrough } = require("stream");
 const portAudio = require("naudiodon");
-const ffmpeg = require("fluent-ffmpeg");
 
 class StreamManager {
   /**
    * The audio device we want to be able to connect to
    */
   #device;
-
-  /**
-   * The ffmpeg process that converts our raw audio to the ogg/opus
-   * format that Discord expects
-   */
-  #proc = null;
 
   /**
    * Our PortAudio input connection
@@ -36,22 +29,7 @@ class StreamManager {
       },
     });
 
-    /**
-     * FFMPEG Magic!
-     * This converts the "raw PCM" data output by portAudio into an ogg format,
-     * which is then read by the Discord API. Here's a breakdown:
-     *
-     *   - "s16le"     - signed, 16-bit, little-endian. The format of portAudio.SampleFormat16Bit
-     *   - "-ar"       - the audio sample rate, which we specified above
-     *   - "-ac"       - the number of audio channels
-     *
-     * Breakdown here: https://stackoverflow.com/a/11990796
-     */
-    this.#proc = ffmpeg({ source: this.#ai })
-      .inputFormat("s16le")
-      .inputOptions([`-ar ${this.#device.defaultSampleRate}`, `-ac ${2}`]);
-
-    this.#proc.outputFormat("ogg").pipe(pass);
+    this.#ai.pipe(pass);
     this.#ai.start();
 
     return pass;
@@ -61,10 +39,6 @@ class StreamManager {
     if (this.#ai) {
       this.#ai.quit();
       this.#ai = null;
-    }
-    if (this.#proc) {
-      this.#proc.kill();
-      this.#proc = null;
     }
   }
 }
